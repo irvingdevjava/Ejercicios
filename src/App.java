3ac1c8b42f5104fabff3898d8bd711326b513461
irvingdevjava <irvingspringboot@gmail.com>
2024-07-20 20:02:12 -0600

    Update

--- src/App.java
+++ src/App.java
@@ -1,12 +1,14 @@
+import Ejercicios.CalculadoraFactorial;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // GestionAsientos gestionAsientos= new GestionAsientos();
+        // gestionAsientos.inicio();
+
+        CalculadoraFactorial calculadoraFactorial=new CalculadoraFactorial();
         
 
-        // Saludo saludo=new Saludo();
-        // saludo.setNombre("Irving");
-        // saludo.setApellido("Mendoza");
 
-        // System.out.println(saludo.toString());;
     }
 }
